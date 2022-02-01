4a9b5cfd1c7dff84360aedbc64a7c5c41206b58d
Mikael <dev.kov.mtl@gmail.com>
2022-02-01 16:47:55 -0500

    cart and reducer show the total and ordered items

--- src/containers/Cart/Cart.tsx
+++ src/containers/Cart/Cart.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
+
+const CartContainer = styled.div`
+  height: 100%;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-item: center;
+  margin: 0 auto;
+  padding: 42px 0;
+  & > h1 {
+    text-align: center;
+    margin-bottom: 24px;
+    font-weight: bold;
+  }
+`;
+
+const ItemsContainer = styled.div`
+  flex: 1;
+`;
+
+const CartItem = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const ImgCart = styled.img`
+  width: 75px;
+  height: auto;
+`;
+
+const Total = styled.h2`
+  text-align: center;
+  font-size: 2rem;
+  font-weightL bold;
+`;
+
 function Cart() {
+  const dispatch = useAppDispatch();
   const total = useAppSelector((state) => state.cartReducer.total);
+
   const itemsOrdered = useAppSelector(
     (state) => state.cartReducer.itemsOrdered
   );
+
   return (
-    <div>
-      <h1>Cart</h1>
-      <p>$ {total}</p>
-    </div>
+    <CartContainer>
+      <h1>Your Shopping cart</h1>
+      <ItemsContainer>
+        {itemsOrdered &&
+          itemsOrdered.map((item) => (
+            <CartItem key={item.id}>
+              <ImgCart src={item.image} alt={`${item.title}`} />
+              <div>
+                <h3>{`${item.title.substring(0, 18)}...`}</h3>
+                <p>$ {`${item.price}`}</p>
+              </div>
+            </CartItem>
+          ))}
+      </ItemsContainer>
+
+      <Total>Total: $ {total.toFixed(2)}</Total>
+    </CartContainer>
   );
 }
 
--- src/containers/Cart/cartSlice.ts
+++ src/containers/Cart/cartSlice.ts
@@ -13,11 +13,15 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     add: (state, action: PayloadAction<Iitem>) => {
-      state.total += parseInt(action.payload.price);
-      state.itemsOrdered.concat(action.payload);
+      state.total += action.payload.price;
+      const exist = state.itemsOrdered.find(
+        (item) => item.id === action.payload.id
+      );
+
+      state.itemsOrdered = state.itemsOrdered.concat(action.payload);
     },
     remove: (state, action: PayloadAction<Iitem>) => {
-      state.total -= parseInt(action.payload.price);
+      state.total -= action.payload.price;
       state.itemsOrdered = state.itemsOrdered.filter(
         (item) => item.id !== action.payload.id
       );
@@ -31,5 +35,8 @@ export const total = (state: RootState) => state.cartReducer.total;
 
 export const itemsOrdered = (state: RootState) =>
   state.cartReducer.itemsOrdered;
+
+export const nbrItemsOrdered = (state: RootState) =>
+  state.cartReducer.itemsOrdered.length;
 
 export default cartSlice.reducer;
