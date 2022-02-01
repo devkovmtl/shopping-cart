c27a859c9acb0aebeef36c21b124617b27602e20
Mikael <dev.kov.mtl@gmail.com>
2022-01-31 21:12:39 -0500

    footer component created

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Shops</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from './Header/Header';
 export { default as ItemCard } from './ItemCard/ItemCard';
+export { default as Footer } from './Footer/Footer';
